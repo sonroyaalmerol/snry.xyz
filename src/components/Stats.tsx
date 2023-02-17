e08030fd6ec2cbe58a039f98920475c09f1de38f
Son Roy Almerol <sonroya.almerol@gmail.com>
2023-02-16 22:44:32 -0500

    feature: add stats description

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,5 +1,5 @@
 import { useTrysteroRoom } from "@/utils/hooks/useTrysteroRoom"
-import { Box, BoxProps, HStack, Hide, VStack, Text } from "@chakra-ui/react"
+import { Box, BoxProps, VStack, Text } from "@chakra-ui/react"
 import React from "react"
 
 const Stats: React.FC<BoxProps> = ({ children, ...restProps }) => {
@@ -8,7 +8,8 @@ const Stats: React.FC<BoxProps> = ({ children, ...restProps }) => {
   return (
     <Box as="footer" position="fixed" bottom="0" left="0" padding="2rem" {...restProps}>
       <VStack align="baseline">
-        <Text fontSize="xs">Current visitors: {peers.length + 1}</Text>
+        <Text fontSize="xs" maxW="50vw">Everyone on this page will be able to connect to others in real-time, synchronising mouse movements.</Text>
+        <Text fontSize="xs">Page Visitor Count: {peers.length + 1}</Text>
       </VStack>
     </Box>
   )
